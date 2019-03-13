5b45e017334b7bca98fa58a5bc8eafdb9cf8e40f
Oscar Barrera <oscbco@gmail.com>
2019-03-12 22:35:13 -0600

    Added new test: Show an element from the list after clicking the select

--- source/Select/Select.test.js
+++ source/Select/Select.test.js
@@ -13,10 +13,30 @@ describe('inline-select', () => {
     );
     expect(getByText('Please select option')).toBeInTheDocument();
   });
-  /*
-  it('renders 1 <Select /> components', () => {
-    const wrapper = shallow(<Select />);
-    expect(wrapper.text()).toEqual('Please select option ');
+
+  it('Shows an element from the list after clicking the select ', () => {
+    const fontSizes = [
+      {
+        label: '9px',
+        value: '9'
+      }, {
+        label: '10px',
+        value: '10'
+      }, {
+        label: '11px',
+        value: '11'
+      }, {
+        label: '12px',
+        value: '12'
+      }
+    ];
+
+    const { getByText, getByTestId, container, asFragment } = render(
+      <Select defaultSelected='english' optionList={fontSizes} />
+    );
+    expect(getByText('Please select option')).toBeInTheDocument();
+    fireEvent.click(getByText(/Please select option/i));
+    expect(getByText('Please select option')).toBeInTheDocument();
+    expect(getByText('12px')).toBeInTheDocument();
   });
-  */
 });
