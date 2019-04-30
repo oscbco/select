30e7d134d93db4250e88d8622d3b2cef01ec966d
Oscar Barrera <oscbco@gmail.com>
2019-04-29 23:33:58 -0600

    New tests

--- source/InlineSelect/InlineSelect.test.js
+++ source/InlineSelect/InlineSelect.test.js
@@ -1,42 +1,53 @@
 import React from 'react';
-import { render, fireEvent, cleanup, waitForElement } from 'react-testing-library';
+import { render, fireEvent, cleanup } from 'react-testing-library';
 import 'jest-dom/extend-expect';
 
 import InlineSelect from './InlineSelect';
 
+const fontSizes = [
+  {
+    label: '9px',
+    value: '9'
+  }, {
+    label: '10px',
+    value: '10'
+  }, {
+    label: '11px',
+    value: '11'
+  }, {
+    label: '12px',
+    value: '12'
+  }
+];
+
 afterEach(cleanup);
 
 describe('inline-select', () => {
-  it('renders 1 <InlineSelect /> components', () => {
-    const { getByText, getByTestId, container, asFragment } = render(
-      <InlineSelect />
-    );
-    expect(getByText('Select option')).toBeInTheDocument();
+  it('shows correct placeholder', () => {
+    const { queryAllByText } = render(<InlineSelect />);
+    expect(queryAllByText('Select option').length).toBe(1);
   });
 
-  it('Shows an element from the list after clicking the select ', () => {
-    const fontSizes = [
-      {
-        label: '9px',
-        value: '9'
-      }, {
-        label: '10px',
-        value: '10'
-      }, {
-        label: '11px',
-        value: '11'
-      }, {
-        label: '12px',
-        value: '12'
-      }
-    ];
+  it('Shows correct title after clicking on item "9px" ', () => {
+    const { queryByText, queryAllByText } = render(<InlineSelect defaultItem='english' items={fontSizes} />);
+    expect(queryAllByText('Select option').length).toBe(1);
+    fireEvent.click(queryByText('Select option'));
+    expect(queryAllByText('Select option').length).toBe(1);
+    expect(queryAllByText('9px').length).toBe(1);
+    fireEvent.click(queryByText('9px'));
+    expect(queryAllByText('Select option').length).toBe(0);
+    expect(queryAllByText('9px').length).toBe(2);
+  });
 
-    const { getByText, getByTestId, container, asFragment } = render(
-      <InlineSelect defaultItem='english' items={fontSizes} />
-    );
-    expect(getByText('Select option')).toBeInTheDocument();
-    fireEvent.click(getByText(/Select option/i));
-    expect(getByText('Select option')).toBeInTheDocument();
-    expect(getByText('12px')).toBeInTheDocument();
+  it('Shows correct title after clicking on item "10px" ', () => {
+    const { queryByText, queryAllByText } = render(<InlineSelect defaultItem='english' items={fontSizes} />);
+    expect(queryAllByText('Select option').length).toBe(1);
+    fireEvent.click(queryByText('Select option'));
+    expect(queryAllByText('Select option').length).toBe(1);
+    expect(queryAllByText('9px').length).toBe(1);
+    fireEvent.click(queryByText('10px'));
+    expect(queryAllByText('Select option').length).toBe(0);
+    expect(queryAllByText('9px').length).toBe(1);
+    expect(queryAllByText('10px').length).toBe(2);
   });
 });
